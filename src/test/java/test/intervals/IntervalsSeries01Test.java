b4b7e7201e0786830a04e3a9c73eecc1903ce800
madamjak <madamjak@marian-ntb-w.adamjak.local>
2020-10-14 11:38:28 +0200

    fix junit dependency

--- src/test/java/test/intervals/IntervalsSeries01Test.java
+++ src/test/java/test/intervals/IntervalsSeries01Test.java
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package test.java.net.adamjak.intervals;
+package test.intervals;
 
 import java.util.ArrayList;
 import java.util.Collection;
